053ac5a496aeff68db33ba5968e1abe66e01fa66
Aliyu Habibu <aliyuhabibu111@gmail.com>
2024-01-23 01:39:36 +0100

    Understand preprocessor

---
+++ 0x0D-preprocessor/02-main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - do my task
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	printf(__FILE__);
+	putchar(10);
+	return (0);
+}
---
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -0,0 +1,9 @@
+#ifndef FUNCTION_LIKE_MACRO_H
+#define FUNCTION_LIKE_MACRO_H
+
+#include <stdio.h>
+
+#define ABS(x) (x * -1) if (x < 0)
+
+
+#endif /* FUNCTION_LIKE_MACRO_H */
