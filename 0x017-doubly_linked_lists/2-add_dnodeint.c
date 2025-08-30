c55e226bed815526cf4e85e71c816681ff53cc9f
Aliyu Habibu <aliyuhabibu111@gmail.com>
2025-08-30 13:48:36 +0100

    Remove redundancy

--- 0x017-doubly_linked_lists/2-add_dnodeint.c
+++ 0x017-doubly_linked_lists/2-add_dnodeint.c
@@ -20,7 +20,6 @@ dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 		new->next = NULL;
 		new->prev = NULL;
 		*head = new;
-		/*return new;*/
 	}
 	else
 	{
@@ -29,7 +28,6 @@ dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 		new->prev = NULL;
 		(*head)->prev = new;
 		*head = new;
-		/*return new;*/
 	}
 	return (new);
 }
